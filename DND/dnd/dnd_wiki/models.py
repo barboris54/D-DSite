29ced14c706542484c1b810699b7e15eb3bd7804
barboris54 <barboris229@mail.ru>
2023-05-02 22:20:06 +0300

    DZ_4

--- DND/dnd/dnd_wiki/models.py
+++ DND/dnd/dnd_wiki/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class HeroesClass(models.Model):
+    rus_name = models.CharField(max_length=200)
+    eng_name = models.CharField(max_length=200)
+    adons_name = models.CharField(max_length=200)
+    bg_image = models.ImageField(upload_to='dnd_wiki/icons', default='')
+
+    def __str__(self):
+        return self.rus_name
--- DND/dnd/dnd_wiki/views.py
+++ DND/dnd/dnd_wiki/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import HeroesClass
 
 def main_page(request):
-    return render(request, 'dnd_wiki/main_page.html')
+    pr = HeroesClass.objects.all()
+    context = {'heroes': pr}
+    return render(request, 'dnd_wiki/main_page.html', context)
