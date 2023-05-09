9782d8986abf9e9a91a544f57a3cc1c3565776d6
barboris54 <barboris229@mail.ru>
2023-05-09 14:32:24 +0300

    Commit 6_1

--- DND/dnd/dnd_wiki/admin.py
+++ DND/dnd/dnd_wiki/admin.py
@@ -4,3 +4,4 @@ from .models import HeroesClass, Companions, ClassHomebrew
 admin.site.register(HeroesClass)
 admin.site.register(Companions)
 admin.site.register(ClassHomebrew)
+
--- DND/dnd/dnd_wiki/models.py
+++ DND/dnd/dnd_wiki/models.py
@@ -1,11 +1,37 @@
 from django.db import models
 
-
 class HeroesClass(models.Model):
     rus_name = models.CharField(max_length=200)
     eng_name = models.CharField(max_length=200)
     adons_name = models.CharField(max_length=200)
     bg_image = models.ImageField(upload_to='dnd_wiki/icons', default='', blank=True)
+    name = models.CharField(max_length=200, default="default title")
+    source = models.CharField(max_length=200, default="default title")
+    lore_info = models.TextField(null=True, blank=True, default="default title")
+    short_info = models.CharField(max_length=200, default="default title")
+    table = models.TextField(null=True, blank=True, default="default title")
+    description_title1 = models.CharField(max_length=200, null=True, blank=True, default="default title")
+    description_info1 = models.TextField(null=True, blank=True, default="default title")
+    description_title2 = models.CharField(max_length=200, null=True, blank=True, default="default title")
+    description_info2 = models.TextField(null=True, blank=True, default="default title")
+    fast_creations = models.TextField(null=True, blank=True, default="default title")
+    creations = models.TextField(default="default title")
+    deep_lore_decs = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    deep_lore_min_char = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    deep_lore_use = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    deep_lore_ability = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    class_ability_desc = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    class_ability_hp = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    class_ability_hp_1lvl = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    class_ability_hp_nextlvl = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    class_ability_armor = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    class_ability_weapon = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    class_ability_tools = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    class_ability_safedice = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    class_ability_equipment1 = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    class_ability_equipment2 = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    class_ability_equipment3 = models.CharField(null=True, blank=True, default="default title", max_length=200)
+    # законичл классовые умения
 
     def __str__(self):
         return self.rus_name
@@ -26,3 +52,5 @@ class ClassHomebrew(models.Model):
     adons_name2 = models.CharField(max_length=200, blank=True)
     adons_name3 = models.CharField(max_length=200, blank=True)
     bg_image = models.ImageField(upload_to='dnd_wiki/icons', default='', blank=True)
+
+
